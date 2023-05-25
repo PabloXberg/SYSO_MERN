77e5a93a6abd2e40b1f0b75884914a58b9d216b7
PabloXberg <pabloxberg@gmail.com>
2023-05-25 14:26:59 +0200

    intentando terminar el front-end de los likes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ import Login from './pages/Login';
 import { AuthContext, AuthContextProvider } from './contexts/AuthContext';
 import Navbar from './components/Navbar';
 import NavStrap from './components/NavTrap';
-import Events from './pages/events';
+
 import Sketches from './pages/sketches';
 import UsersPage from './pages/users';
 import MySketchs from './pages/mySketchs';
@@ -26,7 +26,7 @@ function App() {
                     <Route path='/' element={ <Homepage />} />
                     <Route path='register' element={ <Register /> } />
                     <Route path='login' element={<Login />} />
-                    <Route path='events' element={<Events />} />
+
                     <Route path='sketches' element={<Sketches />} />
                     <Route path='users' element={<UsersPage />} />
                     <Route path='mysketchs' element={<MySketchs />} />
--- client/src/components/SketchCard.tsx
+++ client/src/components/SketchCard.tsx
@@ -6,12 +6,74 @@ import UserModal from '../components/UserModal'
 
 // import UserModel from "../../../server/models/userModels.js";
 
+
+
 function SketchCard(props: any) {
 const { user, login, logout } = useContext(AuthContext);
    const [show, setShow] = useState(false);
 
+    const datum = props.props.createdAt;
+    const shortdatum = datum.substring(0, 10);
+  
+    const [numberLikes, setNumberLikes] = useState<any>(props.props.likes.lenght ? props.props.likes.lenght : 0)
     
- 
+    // console.log('props.props.likes.lenght  :>> ', props.props.likes[0] );
+    // console.log('numberLikes :>> ', numberLikes);
+
+const likeSketch = async (_id: any) => {
+  
+const myHeaders = new Headers();
+    
+myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+myHeaders.append("Authorization", `Bearer ${localStorage.getItem("token")}`);
+
+const urlencoded = new URLSearchParams();
+urlencoded.append("sketch", _id);
+
+const requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: urlencoded,
+};
+    
+try {
+    const response = await fetch("http://localhost:5000/api/sketches/like", requestOptions)
+    const result = await response.json();
+    console.log(result);
+
+  
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
+  
+const unlikeSketch = async (_id: any) => {
+  
+const myHeaders = new Headers();
+    
+myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+myHeaders.append("Authorization", `Bearer ${localStorage.getItem("token")}`);
+
+const urlencoded = new URLSearchParams();
+urlencoded.append("sketch", _id);
+
+const requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: urlencoded,
+};
+    
+try {
+    const response = await fetch("http://localhost:5000/api/sketches/unlike", requestOptions)
+    const result = await response.json();
+    console.log(result);
+  
+    } catch (error) {
+      console.log('error', error)
+    }
+}
+  
+  
   return (
     <Card style={{ width: '18rem', height:'auto' }}>
       <Card.Img variant="top" alt ="Sketch" src={props.props.url} />
@@ -21,17 +83,25 @@ const { user, login, logout } = useContext(AuthContext);
           {props.props.comment ? props.props.comment : "Hier we can see some info about the Sketch"}
         </Card.Text>
         
-   
-        {/* <Button variant="primary">Details</Button> */}
-        {props.props.owner.username ? <Card.Footer className="text-muted"> <i>Created by: </i> <Card.Link style={{ cursor: "pointer" }} onClick={() => setShow(true)}><b>{props?.props.owner.username ? props?.props.owner.username : user?.username}</b></Card.Link></Card.Footer> : <Card.Footer><i className="material-icons" style={{cursor: "pointer"}}>delete_forever</i></Card.Footer>}
-              {/* <Card.Footer className="text-muted"><i>Registered on: {shortdatum}</i></Card.Footer>     ME FALTA AGREGAR EL TIMESTAP CUANDO TENGA LA OPCION DE UPLOAD A NEW IMAGE*/} 
-        <Card.Footer>
-          
-          <i className="material-icons" style={{cursor: "pointer"}}>thumb_up</i>
-          <i className="material-icons" style={{cursor: "pointer"}}>thumb_down</i>
-         
-            <i className="material-icons" style={{cursor: "pointer"}}>message</i>
-       </Card.Footer>
+        {props.props.owner.username ?
+          <Card.Footer className="text-muted"> <i>Created by: </i> <Card.Link style={{ cursor: "pointer" }} onClick={() => setShow(true)}><b>{props?.props.owner.username ? props?.props.owner.username : user?.username}</b></Card.Link> 
+
+          </Card.Footer> : <Card.Footer><i className="material-icons" style={{ cursor: "pointer" }}>delete_forever</i></Card.Footer>}
+          <Card.Footer className="text-muted"><i>Upload: {shortdatum}</i>
+            
+        </Card.Footer> 
+        <Card.Footer style={{ display: "Flex", flexDirection: "row" }}>
+           <div style={{alignSelf:"flex-start"}}>
+            <i className="material-icons" style={{ cursor: "pointer" }} onClick={likeSketch}>thumb_up</i>
+            <i className="material-icons" style={{ cursor: "pointer" }} onClick={unlikeSketch}>thumb_down</i>
+          </div> 
+                
+          {/* <div style={{alignSelf:"flex-end", justifySelf: "right", justifyContent:"right"}}>
+            <h6>{numberLikes} likes</h6>
+          </div> */}
+               
+          </Card.Footer>  
+       
       </Card.Body>
       <UserModal style={{cursor: "pointer"}} onClose={()=> setShow(false)} show={show} character={props.props} />
     </Card>
