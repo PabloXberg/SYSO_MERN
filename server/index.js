9b5c597db76cd135201946e94aaa89f4efaf0cca
PabloXberg <pabloxberg@gmail.com>
2023-06-25 17:44:01 +0200

    origin for deploy

--- server/index.js
+++ server/index.js
@@ -20,7 +20,23 @@ const setMiddlewares = () => {
     extended: true,
   })
   );
-  app.use(cors());
+
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://share-your-sketch-online.vercel.app",
+  ];
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  };
+
+  // app.use(cors());
+  app.use(cors(corsOptions));
   cloudinaryConfig();
   passportConfig();
 }
