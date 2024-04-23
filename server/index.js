12d53f3fad892a8019808c00851ad5bd076c1356
PabloXberg <pabloxberg@gmail.com>
2024-04-23 18:12:33 +0200

    probando con bodyParser

--- server/index.js
+++ server/index.js
@@ -66,13 +66,12 @@ import passportConfig from "./config/passport.js";
 const app = express();
 const port = process.env.PORT || 5000;
 
-const setMiddlewares =() => {
-  app.use(express.json());
-  app.use(
-    express.urlencoded({
-      extended: true,
-    })
-  );
+const setMiddlewares = () => {
+  
+  // app.use(express.json());
+  // app.use(express.urlencoded({ extended: true, }));
+  app.use(bodyParser.json()); app.use(bodyParser.urlencoded({ extended: true }));
+
   app.use(cors());
   cloudinaryConfig();
   passportConfig();
