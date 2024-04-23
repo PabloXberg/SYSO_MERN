37a4c09535cda009b248d2371bb7ef106a8f91e0
PabloXberg <pabloxberg@gmail.com>
2024-04-23 17:53:10 +0200

    probando con otro index.js

--- server/index.js
+++ server/index.js
@@ -1,61 +1,97 @@
+// import express from "express";
+// import mongoose from "mongoose";
+// import * as dotenv from "dotenv";
+// dotenv.config();
+// import userRouter from "./routes/userRoutes.js";
+// import sketchRouter from "./routes/sketchRoutes.js";
+// import cloudinaryConfig from "./config/cloudinary.js";
+// import cors from "cors";
+// import passportConfig from "./config/passport.js";
+// import commentRouter from "./routes/commentsRoutes.js";
+
+
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// const setMiddlewares = () => {
+//   app.use(express.json());
+//   app.use(
+//   express.urlencoded({
+//     extended: true,
+//   })
+//   );
+//   app.use(cors());
+//   cloudinaryConfig();
+//   passportConfig();
+// }
+
+// const connectMongoose = () => {
+//   mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     app.listen(port, () => {
+//       console.log("Connection to MongoDB established, and server is running on port " + port);
+//     });
+//   })
+//   .catch((err) => console.log(err));
+// }
+
+// const connectRoutes = () => {
+//   app.use("/api/users", userRouter);
+//   app.use("/api/sketches", sketchRouter);
+//   app.use("/api/comments", commentRouter);
+//   app.use("*", (req,res) => {res.status(500).json({error: "Endpoint not found - What for a API is that!?!?!"})})
+// }
+
+
+// setMiddlewares();
+// connectMongoose();
+// connectRoutes();
+
 import express from "express";
 import mongoose from "mongoose";
+import cloudinaryConfig from "./config/cloudinary.js";
 import * as dotenv from "dotenv";
 dotenv.config();
-import userRouter from "./routes/userRoutes.js";
-import sketchRouter from "./routes/sketchRoutes.js";
-import cloudinaryConfig from "./config/cloudinary.js";
+
+import userRouter from './routes/userRoutes.js'
+
 import cors from "cors";
+import petRouter from "./routes/petRoutes.js";
 import passportConfig from "./config/passport.js";
-import commentRouter from "./routes/commentsRoutes.js";
 
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-const setMiddlewares = () => {
+const setMiddlewares =() => {
   app.use(express.json());
   app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+      extended: true,
+    })
   );
-
-  const allowedOrigins = ["http://localhost:3000", "https://shareyoursketch.vercel.app"];
-  const corsOptions = {
-    origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not Allowed by CORS"));
-      }
-    },
-  };
-
   app.use(cors());
-  // app.use(cors(corsOptions));
   cloudinaryConfig();
   passportConfig();
 }
 
 const connectMongoose = () => {
   mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(port, () => {
-      console.log("Connection to MongoDB established, and server is running on port " + port);
-    });
-  })
-  .catch((err) => console.log(err));
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      app.listen(port, () => {
+        console.log("Connection to MongoDB established, and server is running on port " + port);
+      });
+    })
+    .catch((err) => console.log(err));
 }
 
 const connectRoutes = () => {
-  app.use("/api/users", userRouter);
-  app.use("/api/sketches", sketchRouter);
-  app.use("/api/comments", commentRouter);
-  app.use("*", (req,res) => {res.status(500).json({error: "Endpoint not found - What for a API is that!?!?!"})})
+  app.use('/api/users', userRouter);
+  app.use('/api/pets', petRouter);
+  app.use('*', (req, res) => { res.status(500).json({ error: "Endpoint not found" }) });
 }
-
 
 setMiddlewares();
 connectMongoose();
