95885360c765645180e5414d1acc803cee646bc8
PabloXberg <pabloxberg@gmail.com>
2024-09-11 20:17:21 +0200

    cambio de tecto, batalla actual, mejorado

--- client/src/pages/actualbattle.tsx
+++ client/src/pages/actualbattle.tsx
@@ -17,7 +17,7 @@ const ActualBattle = () => {
         <div className="battleText">
           <h2 className="tituloFuente">Share your Sketch Battle # 1</h2>
       
-          <h4>
+          <h5>
             Os damos la bienvenida a esta primera batalla de bocetos, donde todo
             participante es mas que bien bienvenido!
             <br />
@@ -34,7 +34,7 @@ const ActualBattle = () => {
             batalla.
             <br />
             
-            <h2 className="tituloFuente">Reglas del concurso: </h2>
+            <h3 className="tituloFuente">Reglas del concurso: </h3>
         
             * Palabra clave "SHARE"
             <br />
@@ -43,15 +43,15 @@ const ActualBattle = () => {
             <br />
             * Registrarse en esta Web antes de la fecha indicada. <br />
            
-            <h2 className="tituloFuente">Premios: </h2>
+            <h3 className="tituloFuente">Premios: </h3>
         
             * Camiseta parte frontal y trasera <br />
                       * logotipo del Share your Sketch, tambien un Poster y una lamina Din A4.<br />
             * Blackbook formato, Lote de pegatinas, Pack de lapices y figura de
             impresion 3D del logo.
           
-            <h2 className="tituloFuente">Requisitos:
- </h2>
+            <h3 className="tituloFuente">Requisitos:
+ </h3>
           
             Registrarse en esta pagina ademas de seguirnos en Instagram, subir
             el boceto con la descripcion Sketch Battle #1 antes del día 30 de
@@ -59,13 +59,13 @@ const ActualBattle = () => {
             tambien subir en Stories de Instagram añadiendo el enlace de esta
             pagina con el titular ‘Sketch Battle’
                       <br />
-                    <h2 className="tituloFuente">Jueces:
- </h2>  
+                    <h3 className="tituloFuente">Jueces:
+ </h3>  
 AliciaNuro - 
 Sig. Luigi - 
 SoldOne
 
-          </h4>
+          </h5>
         </div>
       </div>
     </>
