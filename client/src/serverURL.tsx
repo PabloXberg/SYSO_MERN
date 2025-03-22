5d699063726935f0f77ce6ce8b7c2d5dd0d32a59
PabloXberg <pabloxberg@gmail.com>
2025-03-22 20:01:33 +0100

    changing port to 3000

--- client/src/serverURL.tsx
+++ client/src/serverURL.tsx
@@ -1,6 +1,6 @@
 const serverURL =
     process.env.NODE_ENV === "development"
-        ? "http://localhost:5000/api/"
+        ? "http://localhost:3000/api/"
         : "https://shareyourserver.vercel.app/api/";
 
 console.log('serverURL :>> ', serverURL);
