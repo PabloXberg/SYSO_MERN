91a405d950ebd95dd49235962957957b792d58c6
PabloXberg <pabloxberg@gmail.com>
2025-04-11 15:51:57 +0200

    url del server de coolify

--- client/src/serverURL.tsx
+++ client/src/serverURL.tsx
@@ -1,12 +1,12 @@
-const serverURL =
-    process.env.NODE_ENV === "development"
-        ? "http://localhost:5000/api/"
-        : "http://hskwk44k04kwk00ow4g40oo0.91.108.113.166.sslip.io/api/"
-    ;
+// const serverURL =
+//     process.env.NODE_ENV === "development"
+//         ? "http://localhost:5000/api/"
+//         : "http://hskwk44k04kwk00ow4g40oo0.91.108.113.166.sslip.io/api/"
+//     ;
 
-console.log('serverURL :>> ', serverURL);
+// console.log('serverURL :>> ', serverURL);
 
-export { serverURL };
+// export { serverURL };
 
 
 
@@ -21,27 +21,27 @@ export { serverURL };
 // export { serverURL };
 
 
-// let serverURL: string;
+let serverURL: string;
 
-// if (process.env.NODE_ENV === "development") {
-//     // Distintas opciones de servidor dentro de "development"
-//     switch (process.env.SERVER_MODE) {
-//         case "local":
-//             serverURL = "http://localhost:5000/api/";
-//             break;
-//         case "remote-dev":
-//             serverURL = "http://192.168.1.10:5000/api/";
-//             break;
-//         case "docker":
-//             serverURL = "http://127.0.0.1:8000/api/";
-//             break;
-//         default:
-//             serverURL = "http://localhost:5000/api/";
-//     }
-// } else {
-//     serverURL = "https://shareyourserver.vercel.app/api/";
-// }
+if (process.env.NODE_ENV === "development") {
+    // Distintas opciones de servidor dentro de "development"
+    switch (process.env.SERVER_MODE) {
+        case "local":
+            serverURL = "http://shareyoursketch.vercel.app/api/";
+            break;
+        case "production":
+            serverURL = "http://hskwk44k04kwk00ow4g40oo0.91.108.113.166.sslip.io/api/";
+            break;
+        case "development":
+            serverURL = "http://localhost:5000/api/";
+            break;
+        default:
+            serverURL = "http://localhost:5000/api/";
+    }
+} else {
+    serverURL = "http://hskwk44k04kwk00ow4g40oo0.91.108.113.166.sslip.io/api/";
+}
 
-// console.log('serverURL :>> ', serverURL);
+console.log('serverURL :>> ', serverURL);
 
-// export { serverURL };
+export { serverURL };
