b90fccae2031060c7eaeab595bb42f9f035c19a4
PabloXberg <pabloxberg@gmail.com>
2025-04-11 14:00:52 +0200

    url del server de coolify

--- client/src/serverURL.tsx
+++ client/src/serverURL.tsx
@@ -1,26 +1,21 @@
-const serverURL =
-    process.env.NODE_ENV === "development"
-        ? "http://localhost:5000/api/"
-        : "https://shareyourserver.vercel.app/api/"
-    ;
-
-console.log('serverURL :>> ', serverURL);
-
-export { serverURL };
-
-
-
-
-// let serverURL;
-
-// if (process.env.NODE_ENV === "development") {
-//     serverURL = "http://localhost:5000/api/";
-// } else if (process.env.CUSTOM_ENV === "staging") {
-//     serverURL = "https://staging.shareyourserver.com/api/";
-// } else {
-//     serverURL = "https://shareyourserver.vercel.app/api/";
-// }
+// const serverURL =
+//     process.env.NODE_ENV === "development"
+//         ? "http://localhost:5000/api/"
+//         : "https://shareyourserver.vercel.app/api/"
+//     ;
 
 // console.log('serverURL :>> ', serverURL);
 
 // export { serverURL };
+
+
+
+
+const serverURL: string =
+    process.env.NODE_ENV === "development"
+        ? "http://localhost:5000/api/"
+        : process.env.CUSTOM_ENV === "staging"
+        ? "http://hskwk44k04kwk00ow4g40oo0.91.108.113.166.sslip.io"
+        : "https://shareyourserver.vercel.app/api/";
+
+export { serverURL };
